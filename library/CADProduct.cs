e760be94f6af77eed9660ecb01c68cf96b45e071
Ariadna Miralles González <amg538@mscloud.ua.es>
2026-03-13 12:06:48 +0100

    20518810G atributos ENProduct y CADProduct

--- library/CADProduct.cs
+++ library/CADProduct.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace library
 {
     internal class CADProduct
     {
+        private string constring;   //cadena a la base de datos
+        public CADProduct(string direccion)
+        {
+            this.constring = direccion;
+        }
+        /* public bool Create(ENProduct en)
+         {
+             return false;
+         }*/
     }
 }
--- library/ENProduct.cs
+++ library/ENProduct.cs
@@ -8,5 +8,37 @@ namespace library
 {
     internal class ENProduct
     {
+        private string code;   //falta poner propiedades públicas con campo de respaldo para obtener dichos valores
+        private string name;
+        private int amount;
+        private float price;
+        private int categoy;
+        private DateTime creationDate;
+
+        public ENProduct()
+        {
+            code = ""; 
+            name = ""; 
+            amount = 0;
+            price = 0.0f;
+            categoy = 0;
+            creationDate = DateTime.Now;
+        }
+        public ENProduct(string code, string name, int amount, float price, int category, DateTime creationDate)
+        {
+            this.code = code;
+            this.name = name;
+            this.amount = amount;
+            this.price = price;
+            this.categoy = category;
+            this.creationDate = creationDate;
+        }
+       /* public bool Create()
+        {
+            if (error)
+            {
+                return false;
+            }
+        }*/
     }
 }
